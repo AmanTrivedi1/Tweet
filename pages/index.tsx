7b4f74fb4be90c35850ae6eefb64aa26e19815f2
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-05-21 10:54:53 +0530

    getting the username and profilepic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { signOut, useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +9,9 @@ export default function Home() {
   const { data: session, status } = useSession({
     required: true,
   });
+
+  const [username, setUserName] = useState<string>("");
+  const [profileImage, setProfileImage] = useState<string>("/blank_pp.webp");
 
   function makeid(length: number) {
     let result = "";
@@ -22,18 +25,57 @@ export default function Home() {
     return result;
   }
 
+  async function getUser(email: string) {
+    const params = new URLSearchParams({
+      email: email,
+    });
+    const response = await fetch("api/user?" + params);
+    if (response.status === 200) {
+      const data = await response.json();
+      return data;
+    }
+  }
+
+  async function createUser() {
+    const email = session?.user?.email as string;
+    const name = session?.user?.name as string;
+    const username = (session?.user?.name?.split(" ")[0] +
+      "_" +
+      makeid(6)) as string;
+    const response = await fetch("/api/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        name,
+        username,
+        profileImage: "/blank_pp.webp",
+      }),
+    });
+    if (response.status == 200) {
+      const data = response.json();
+      return data;
+    }
+  }
+
   useEffect(() => {
     async function f() {
       if (status != "loading" && session) {
-        const email = session?.user?.email as string;
-        const name = session?.user?.name as string;
-        const username = (session?.user?.name?.split(" ")[0] +
-          "_" +
-          makeid(6)) as string;
+        const data = await getUser(session.user?.email as string);
+        if (data.msg == "new user") {
+          const userData = await createUser();
+          setUserName(userData.user.username);
+          setProfileImage(userData.user.profileImage);
+        } else {
+          setUserName(data.user.username);
+          setProfileImage(data.user.profileImage);
+        }
       }
     }
     f();
-  });
+  }, [status, session]);
 
   if (status == "loading") {
     return <div>Loading...</div>;
@@ -42,8 +84,10 @@ export default function Home() {
   return (
     <>
       <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+        className={`flex min-h-screen flex-col items-center gap-4 p-24 ${inter.className}`}
       >
+        <p>username:{username}</p>
+        {/* <Image /> */}
         <button onClick={() => signOut()}>SignOut</button>
       </main>
     </>
